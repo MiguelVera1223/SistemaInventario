aef4e873d001bd2696ca2f981fb56ce2251aff86
miguel <i.s.mavs1223@gmail.com>
2021-02-11 17:34:31 -0500

    template y static

--- Sistema/Inventario/Inventario/settings.py
+++ Sistema/Inventario/Inventario/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'administracion',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'Inventario.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Inventario',
+        'USER': 'miguel',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
@@ -119,3 +124,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'Inventario/static'))
--- Sistema/Inventario/administracion/models.py
+++ Sistema/Inventario/administracion/models.py
@@ -1,19 +1,36 @@
 from django.db import models
-from django.db.models.fields import related
+
 
 # Create your models here.
 class CategoriaModel(models.Model):
-    categoriaId = models.AutoField(auto_created=True, primary_key=True, unique=True, null=False, db_column='categoria_id')
-    categoriaNombre= models.CharField(max_length=45, db_column='categoria_nombre')
-    categoriaDescripcion= models.CharField(max_length=45, db_column='categoria_descripcion')
+    categoriaId = models.AutoField(
+        auto_created=True, 
+        primary_key=True, 
+        unique=True, 
+        null=False, 
+        db_column='categoria_id')
+    categoriaNombre= models.CharField(
+        max_length=45, 
+        db_column='categoria_nombre')
+    categoriaDescripcion= models.CharField(
+        max_length=45, 
+        db_column='categoria_descripcion')
     class Meta:
         db_table='t_categoria'
         verbose_name_plural = "Categorias"
         verbose_name = "Categoria"
 
 class ProductoModel(models.Model):
-    productoId =models.AutoField(primary_key=True, unique=True, null=False, db_column='producto_id') 
-    categoriaId = models.ForeignKey(CategoriaModel, on_delete=models.PROTECT, db_column='categoria_id', related_name='CategoriaProductos')
+    productoId =models.AutoField(
+        primary_key=True, 
+        unique=True, 
+        null=False, 
+        db_column='producto_id') 
+    categoriaId = models.ForeignKey(
+        CategoriaModel, 
+        on_delete=models.PROTECT, 
+        db_column='categoria_id', 
+        related_name='CategoriaProductos')
     productoCodigo= models.CharField(max_length=45, db_column='producto_codigo')
     productoNombre = models.CharField(max_length=45, db_column='producto_nombre')
     productoStock = models.CharField(max_length=45, db_column='producto_stock')
@@ -27,7 +44,10 @@ class ProductoModel(models.Model):
         verbose_name = "Producto"
 
 class PersonaModel(models.Model):
-    personaId =models.AutoField(primary_key=True, unique=True,null=False, db_column='persona_id')
+    personaId =models.AutoField(
+        primary_key=True, 
+        unique=True,null=False, 
+        db_column='persona_id')
     personaCargo= models.CharField(max_length=45, db_column='persona_cargo')
     personaNombre= models.CharField(max_length=45, db_column='persona_nombre')
     personaDocumento= models.CharField(max_length=45, db_column='persona_documento')
@@ -39,7 +59,10 @@ class PersonaModel(models.Model):
         verbose_name = "Persona" 
 
 class UsuarioModel(models.Model):
-    usuarioId =models.AutoField( primary_key=True, unique=True,null=False, db_column='usuario_id')
+    usuarioId =models.AutoField( 
+        primary_key=True, 
+        unique=True,null=False, 
+        db_column='usuario_id')
     usuarioDocumento= models.CharField(max_length=45, db_column='usuario_documento')
     usuarioCargo= models.CharField(max_length=45, db_column='usuario_cargo')
     usuarioNombre= models.CharField(max_length=45, db_column='usuario_nombre')
@@ -52,13 +75,26 @@ class UsuarioModel(models.Model):
         verbose_name="Usuario"
 
 class MovimientoModel(models.Model):
-    movimientoId =models.AutoField( primary_key=True, unique=True,null=False, db_column='movimiento_id')
+    movimientoId =models.AutoField( 
+        primary_key=True, 
+        unique=True,
+        null=False, 
+        db_column='movimiento_id')
     movimientoTipo = models.CharField(max_length=45, db_column='movimiento_tipo')
     movimientoCantidad = models.IntegerField(db_column='movimiento_cantidad')
     movimientoFecha = models.DateTimeField(db_column='movimiento_fecha')
-    personaId = models.ForeignKey(PersonaModel, on_delete=models.PROTECT, db_column='persona_id', related_name='PersonaMovimiento')
-    usuarioId = models.ForeignKey(UsuarioModel, on_delete=models.PROTECT, db_column='usuario_id', related_name='UsuarioMovimiento')
+    personaId = models.ForeignKey(
+        PersonaModel, 
+        on_delete=models.PROTECT, 
+        db_column='persona_id', 
+        related_name='PersonaMovimiento')
+    usuarioId = models.ForeignKey(
+        UsuarioModel, 
+        on_delete=models.PROTECT,
+        db_column='usuario_id', 
+        related_name='UsuarioMovimiento')
     class Meta:
         db_table='t_movimiento'
         verbose_name_plural = 'Movimientos'
         verbose_name = 'Movimiento'
+        
